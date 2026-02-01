098618fa0a03044da67bc53e474a91afc8132b09
leonardo <leonardoqgaldino@gmail.com>
2026-02-01 16:42:58 -0300

    first commt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import AppRoutes from "./routes";   // arquivo que criamos em src/routes/index.js
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <AppRoutes />
+    </>
   );
 }
 
